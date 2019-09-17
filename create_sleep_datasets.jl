30833353d9108613b2e3e2c8e5c310022a10510b
adelarue <adelarue@mit.edu>
2019-09-17 17:58:11 -0400

    Imputation baseline

--- create_sleep_datasets.jl
+++ create_sleep_datasets.jl
@@ -9,9 +9,21 @@
 using RDatasets, RCall, DataFrames, CSV
 using Random
 
+function nan_to_missing!(df::DataFrame)
+	for name in names(df)
+		df[name] = convert(Vector{Any}, df[name])
+	end
+	for i = 1:nrow(df), name in names(df)
+		if isnan(df[i, name])
+			df[i, name] = missing
+		end
+	end
+end
+
 Random.seed!(1515)
 NUM_IMPUTATIONS = 20
 NOISE = 0.2
+TEST_FRACTION = 0.3
 
 # Get dataset
 R"library(VIM)"
@@ -20,23 +32,15 @@ R"sleep = as.data.frame(scale(sleep))"
 R"library(mice)"
 R"imputed = mice(sleep, m = $NUM_IMPUTATIONS, method='cart')"
 @rget sleep
-
-# Missing values are represented as NAs, change to missing
-data = DataFrame()
-for name in names(sleep)
-    data[name] = []
-end
-for i = 1:nrow(sleep), name in names(sleep)
-    push!(data[name], ifelse(isnan(sleep[i, name]), missing, sleep[i, name]))
-end
+nan_to_missing!(sleep)
+# Train-test split set-up
+num_test_points = Int(floor(nrow(sleep) * TEST_FRACTION))
 
 # Get dependent variable
 for i = 1:NUM_IMPUTATIONS
     R"imputedsleep = complete(imputed, action = $i)"
     @rget imputedsleep
-    data[:Y] = imputedsleep[:Sleep] .+ randn(nrow(imputedsleep)) * NOISE
-    CSV.write("datasets/sleep-$NOISE-$i.csv", data)
+    sleep[:Y] = imputedsleep[:Sleep] .+ randn(nrow(imputedsleep)) * NOISE
+    sleep[:Test] = shuffle(vcat(zeros(Int, nrow(sleep) - num_test_points), ones(Int, num_test_points)))
+    CSV.write("datasets/sleep-$NOISE-$i.csv", sleep)
 end
-
-# Impute missing values using mice with create
-
---
+++ impute-regress.jl
@@ -0,0 +1,37 @@
+###################################
+### impute-regress.jl
+### Script to impute missing values before performing regression
+### Authors: Arthur Delarue, Jean Pauphilet, 2019
+###################################
+
+using RCall, DataFrames, CSV
+
+"""
+	Impute missing values in a dataset using mice
+	Imputes training set alone, then testing set with training set
+"""
+function mice(df::DataFrame)
+	result = deepcopy(df)
+	@rput df
+	R"library(mice)"
+	R"library(dplyr)"
+	R"train = select(subset(as.data.frame(df), Test==0), -c(Test, Y))"
+	# for some reason, sometimes columns are lists
+	R"train = t(apply(train, 1, unlist))"
+	R"imputed = mice(as.data.frame(train), m=1)"
+	R"imputedtrain = complete(imputed, action=1)"
+	@rget imputedtrain
+	R"trainplus = select(as.data.frame(df), -c(Test, Y))"
+	# for some reason, sometimes columns are lists
+	R"trainplus = t(apply(trainplus, 1, unlist))"
+	R"imputedplus = mice(as.data.frame(trainplus), m=1)"
+	R"imputedtest = select(subset(mutate(complete(imputedplus, action=1), Test=df$Test), Test==1), -Test)"
+	@rget imputedtest
+	result[result.Test .== 0, setdiff(names(result), [:Test, :Y])] = imputedtrain
+	result[result.Test .== 1, setdiff(names(result), [:Test, :Y])] = imputedtest
+	return result
+end
+
+data = CSV.read("datasets/sleep-0.2-1.csv")
+println(first(data, 10))
+println(first(mice(data), 10))
