44b378a22423d7c29ab9e50050fc824d6f58744b
adelarue <adelarue@mit.edu>
2019-10-27 22:35:57 -0400

    Added boolean flag to remove zero columns

--- augment.jl
+++ augment.jl
@@ -5,14 +5,30 @@
 ###################################
 
 """
+	Remove columns that are uniformly zero
+"""
+function removezerocolumns(df::DataFrame)
+	tokeep = []
+	for name in names(df)
+		if !all(abs.(df[!, name]) .< 1e-9)
+			push!(tokeep, name)
+		end
+	end
+	return df[!, tokeep]
+end
+
+"""
 	Create the matrix Z such that Z_ij = 1 if X_ij is missing and 0 otherwise
 """
-function indicatemissing(df::DataFrame)
+function indicatemissing(df::DataFrame; removezerocols::Bool=false)
 	result = DataFrame()
 	for name in names(df)
 		if !startswith(String(name), "C") && !(name in [:Y, :Test]) #if not categorical nor Test/Y
 			result[:,Symbol("$(name)_missing")] = (Int.(ismissing.(df[name])))
 		end
+	end
+	if removezerocols
+		result = removezerocolumns(result)
 	end
 	return result
 end
@@ -20,14 +36,17 @@ end
 """
 	Create the outer product between the data matrix and the missingness indicators
 """
-function augmentaffine(df::DataFrame)
+function augmentaffine(df::DataFrame; removezerocols::Bool = false)
 	newdf = zeroimpute(df)
-	Z = indicatemissing(df::DataFrame)
+	Z = indicatemissing(df, removezerocols=true)
 	result = hcat(newdf, Z)
 	for missingname in names(Z)
 		for name in setdiff(names(newdf), [:Test, :Y])
 			result[Symbol("$(name)_$missingname")] = newdf[name] .* Z[missingname]
 		end
+	end
+	if removezerocols
+		result = removezerocolumns(result)
 	end
 	return result
 end
