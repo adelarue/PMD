cb478af87653b6fc159a392f82181a4f740a8d8f
adelarue <adelarue@mit.edu>
2019-10-27 23:30:11 -0400

    Add glmnet penalty factor + helper functions to count missing values

--- regress.jl
+++ regress.jl
@@ -14,17 +14,27 @@ using Statistics
 	Parameters:
 		- lasso: true if we use lasso (penalty chosen using cross-validation),
 				 false if we do not regularize
+		- alpha: elastic net parameter (1.0 for lasso, 0.0 for ridge)
+		- missing_penalty: how much to penalize augmented columns
+				 (columns that contain missing in their name). Regular columns have penalty 1.0
 	Returns:
 		- a dataframe with a single row, with the same column names as the data
-		except Y and Test, and an additional Offset containing the constant term
+		except Test, and an additional Offset containing the constant term
 """
-function regress(Y::Array{Float64}, df::DataFrame; lasso::Bool=false)
+function regress(Y::Array{Float64}, df::DataFrame;
+				 lasso::Bool=false, alpha::Real=0.8, missing_penalty::Real=1.0)
 	cols = setdiff(names(df), [:Test])
 	X = convert(Matrix, df[df[:Test] .== 0, cols])
 	y = convert(Array, Y[df[:Test] .== 0])
 	coefficients = DataFrame()
 	if lasso
-		cv = glmnetcv(X, y, alpha=.8)
+		penalty_factor = ones(length(cols))
+		for (i, col) in enumerate(cols)
+			if occursin("_missing", string(col))
+				penalty_factor[i] = missing_penalty
+			end
+		end
+		cv = glmnetcv(X, y, alpha=alpha, penalty_factor=penalty_factor)
 		for (i, col) in enumerate(cols)
 			coefficients[col] = ([cv.path.betas[i, argmin(cv.meanloss)]])
 		end
