a7b1f2a02bbc69923eb934fd61646f92e8963db7
Jean PAUPHILET <jean.pauphilet@gmail.com>
2020-05-19 12:00:57 +0200

    Fix mice when imputation fails

--- src/impute.jl
+++ src/impute.jl
@@ -5,12 +5,24 @@
 ###################################
 
 using RCall, DataFrames, CSV
+"""
+	Standardize column names R-style
+"""
+function standardize_colnames(df::DataFrame)
+	aux = select(df, Not(:Id))
+	R"library(dplyr)"
+	R"train = $aux"
+	R"colnames <- names(train)"
+	R"names(train) <- make.names(colnames, unique=TRUE)"
+	@rget train
+	train[!,:Id] = df[:,:Id]
+	return train
+end
 
 """
 	Impute missing values in a dataset using mice
-	Imputes training set alone, then testing set with training set
 """
-function mice(df::DataFrame)
+function mice(df::DataFrame; m=2, maxit=5)
 	aux = select(df, Not(:Id))
 	R"library(mice)"
 	R"library(dplyr)"
@@ -18,7 +30,7 @@ function mice(df::DataFrame)
 
 	R"colnames <- names(train)"
 	R"names(train) <- make.names(colnames, unique=TRUE)"
-	R"imputed = mice(as.data.frame(train), m=2, printFlag=F)"
+	R"imputed = mice(as.data.frame(train), m=$m, maxit=$maxit, printFlag=F)"
 	R"imputedtrain = complete(imputed, action=1)"
 	R"names(imputedtrain) <- colnames"
 	@rget imputedtrain
@@ -32,6 +44,17 @@ function mice(df::DataFrame)
 	# result[result.Test .== 0, setdiff(names(result), [:Test])] .= imputedtrain
 	# result[result.Test .== 1, setdiff(names(result), [:Test])] .= imputedtest
 	return imputedtrain
+end
+
+function mice_bruteforce(df::DataFrame; m=2, maxit=5)
+	df_imputed = PHD.mice(df, m=m, maxit=maxit)
+	if any([mean(ismissing.(df_imputed[:,k])) > 0 for k in names(df)]) #If some columns are still missing
+        df_imputed = PHD.mice(df_imputed, m=5, maxit=30) #Reimpute with more effort
+    end
+    if any([mean(ismissing.(df_imputed[:,k])) > 0 for k in names(df)]) #If still some columns are still missing
+        select!(df_imputed, Not([k for k in names(df) if mean(ismissing.(df_imputed[:,k])) > 0])) #Drop
+    end
+	return df_imputed
 end
 
 """
