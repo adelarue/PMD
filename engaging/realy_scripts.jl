4766b70eccf556af6a856f095283ea474547c8ea
Jean PAUPHILET <jean.pauphilet@gmail.com>
2020-05-22 12:16:57 +0200

    Fix multi-class real Y

--- engaging/realy_scripts.jl
+++ engaging/realy_scripts.jl
@@ -33,91 +33,100 @@ for ARG in ARGS
     Y = convert(Array{Float64}, DataFrame(CSV.read("../datasets/"*dname*"/Y.csv", missingstrings=["", "NaN"]))[:,:target])
     test_prop = .3
 
-    ntargets = Base.size(DataFrame(CSV.read("../datasets/"*dname*"/Y.csv", missingstrings=["", "NaN"])),2)
-    @show ntargets
-    if ntargets <= 2
-        for iter in 1:10
-            results_table = similar(results_main,0)
+    target_list = names(DataFrame(CSV.read("../datasets/"*dname*"/Y.csv", missingstrings=["", "NaN"])))
+    @show length(target_list)
 
-            filename = string(dname, "_real_Y", "_$iter.csv")
+    Y = zeros(Base.size(X_missing,1))
+    if length(target_list) <= 2
+        target_name = setdiff(target_list, [:Id])[1]
+        Y = convert(Array{Float64}, DataFrame(CSV.read("../datasets/"*dname*"/Y.csv", missingstrings=["", "NaN"]))[:,target_name])
+    else
+        setdiff!(target_list, [:Id, :target])
+        sort!(target_list)
+        Y = convert(Array{Float64}, DataFrame(CSV.read("../datasets/"*dname*"/Y.csv", missingstrings=["", "NaN"]))[:,target_list[1]])
+    end
 
-            Random.seed!(2142+767*iter)
-            # Split train / test
-            test_ind = rand(nrow(X_missing)) .< test_prop ;
+    for iter in 1:10
+        results_table = similar(results_main,0)
 
-            ## Method 0
-            df = X_missing[:,.!canbemissing]
-            df[!,:Test] = test_ind
-            linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
-            R2, OSR2 = PHD.evaluate(Y, df, linear)
-            push!(results_table, [dname, iter, "Complete Features", OSR2])
-            CSV.write(savedir*filename, results_table)
+        filename = string(dname, "_real_Y", "_$iter.csv")
 
-            ## Method 1.1
-            X_imputed = PHD.mice_bruteforce(X_missing);
-            df = deepcopy(X_imputed)
-            df[!,:Test] = test_ind
-            linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
-            R2, OSR2 = PHD.evaluate(Y, df, linear)
-            push!(results_table, [dname, iter, "Imp-then-Reg 1", OSR2])
-            CSV.write(savedir*filename, results_table)
+        Random.seed!(2142+767*iter)
+        # Split train / test
+        test_ind = rand(nrow(X_missing)) .< test_prop ;
 
-            ## Method 1.2
-            df = deepcopy(X_missing)
-            X_train_imputed = PHD.mice_bruteforce(df[.!test_ind,:]);
-            select!(df, names(X_train_imputed))
-            df[.!test_ind,:] .= X_train_imputed
-            X_all_imputed = PHD.mice(df);
-            df = deepcopy(X_all_imputed)
-            df[!,:Test] = test_ind
-            linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
-            R2, OSR2 = PHD.evaluate(Y, df, linear)
-            push!(results_table, [dname, iter, "Imp-then-Reg 2", OSR2])
-            CSV.write(savedir*filename, results_table)
+        ## Method 0
+        df = X_missing[:,.!canbemissing]
+        df[!,:Test] = test_ind
+        linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
+        R2, OSR2 = PHD.evaluate(Y, df, linear)
+        push!(results_table, [dname, iter, "Complete Features", OSR2])
+        CSV.write(savedir*filename, results_table)
 
-            ## Method 1.3
-            df = deepcopy(X_missing)
-            X_train_imputed = PHD.mice_bruteforce(df[.!test_ind,:]);
-            X_all_imputed = PHD.mice_bruteforce(df[:,names(X_train_imputed)]);
-            select!(df, names(X_train_imputed))
-            df[.!test_ind,:] .= X_train_imputed
-            df[test_ind,:] .= X_all_imputed[test_ind,:]
-            df[!,:Test] = test_ind
-            linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
-            R2, OSR2 = PHD.evaluate(Y, df, linear)
-            push!(results_table, [dname, iter, "Imp-then-Reg 3", OSR2])
-            CSV.write(savedir*filename, results_table)
+        ## Method 1.1
+        X_imputed = PHD.mice_bruteforce(X_missing);
+        df = deepcopy(X_imputed)
+        df[!,:Test] = test_ind
+        linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
+        R2, OSR2 = PHD.evaluate(Y, df, linear)
+        push!(results_table, [dname, iter, "Imp-then-Reg 1", OSR2])
+        CSV.write(savedir*filename, results_table)
 
-            ## Method 1.4
-            means_df = PHD.compute_mean(X_missing[.!test_ind,:])
-            X_imputed = PHD.mean_impute(X_missing, means_df);
-            df = deepcopy(X_imputed)
-            df[!,:Test] = test_ind
-            linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
-            R2, OSR2 = PHD.evaluate(Y, df, linear)
-            push!(results_table, [dname, iter, "Imp-then-Reg 4", OSR2])
-            CSV.write(savedir*filename, results_table)
+        ## Method 1.2
+        df = deepcopy(X_missing)
+        X_train_imputed = PHD.mice_bruteforce(df[.!test_ind,:]);
+        select!(df, names(X_train_imputed))
+        df[.!test_ind,:] .= X_train_imputed
+        X_all_imputed = PHD.mice(df);
+        df = deepcopy(X_all_imputed)
+        df[!,:Test] = test_ind
+        linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
+        R2, OSR2 = PHD.evaluate(Y, df, linear)
+        push!(results_table, [dname, iter, "Imp-then-Reg 2", OSR2])
+        CSV.write(savedir*filename, results_table)
 
-            ## Method 2: Static Adaptability
-            df = deepcopy(X_missing)
-            df[!,:Test] = test_ind
-            X_augmented = hcat(PHD.zeroimpute(df), PHD.indicatemissing(df, removezerocols=true))
-            linear2, bestparams2 = PHD.regress_cv(Y, X_augmented, lasso=[true],
-                                                    alpha=[0.7,0.8,0.9,1.0],
-                                                    missing_penalty=[2.0,4.0,6.0,8.0,12.0,16.0])
-            R2, OSR2 = PHD.evaluate(Y, X_augmented, linear2)
-            push!(results_table, [dname, iter, "Static", OSR2])
-            CSV.write(savedir*filename, results_table)
+        ## Method 1.3
+        df = deepcopy(X_missing)
+        X_train_imputed = PHD.mice_bruteforce(df[.!test_ind,:]);
+        X_all_imputed = PHD.mice_bruteforce(df[:,names(X_train_imputed)]);
+        select!(df, names(X_train_imputed))
+        df[.!test_ind,:] .= X_train_imputed
+        df[test_ind,:] .= X_all_imputed[test_ind,:]
+        df[!,:Test] = test_ind
+        linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
+        R2, OSR2 = PHD.evaluate(Y, df, linear)
+        push!(results_table, [dname, iter, "Imp-then-Reg 3", OSR2])
+        CSV.write(savedir*filename, results_table)
 
-            ## Method 3: Affine Adaptability
-            df = deepcopy(X_missing)
-            df[!,:Test] = test_ind
-            X_affine = PHD.augmentaffine(df, removezerocols=true)
-            linear3, bestparams3 = PHD.regress_cv(Y, X_affine, lasso=[true], alpha=[0.7,0.8,0.9,1.0],
-                                                  missing_penalty=[2.0,4.0,6.0,8.0,12.0,16.0])
-            R2, OSR2 = PHD.evaluate(Y, X_affine, linear3)
-            push!(results_table, [dname, iter, "Affine", OSR2])
-            CSV.write(savedir*filename, results_table)
-        end
+        ## Method 1.4
+        means_df = PHD.compute_mean(X_missing[.!test_ind,:])
+        X_imputed = PHD.mean_impute(X_missing, means_df);
+        df = deepcopy(X_imputed)
+        df[!,:Test] = test_ind
+        linear, bestparams = PHD.regress_cv(Y, df, lasso=[true], alpha=[0.7,0.8,0.9,1.0])
+        R2, OSR2 = PHD.evaluate(Y, df, linear)
+        push!(results_table, [dname, iter, "Imp-then-Reg 4", OSR2])
+        CSV.write(savedir*filename, results_table)
+
+        ## Method 2: Static Adaptability
+        df = deepcopy(X_missing)
+        df[!,:Test] = test_ind
+        X_augmented = hcat(PHD.zeroimpute(df), PHD.indicatemissing(df, removezerocols=true))
+        linear2, bestparams2 = PHD.regress_cv(Y, X_augmented, lasso=[true],
+                                                alpha=[0.7,0.8,0.9,1.0],
+                                                missing_penalty=[2.0,4.0,6.0,8.0,12.0,16.0])
+        R2, OSR2 = PHD.evaluate(Y, X_augmented, linear2)
+        push!(results_table, [dname, iter, "Static", OSR2])
+        CSV.write(savedir*filename, results_table)
+
+        ## Method 3: Affine Adaptability
+        df = deepcopy(X_missing)
+        df[!,:Test] = test_ind
+        X_affine = PHD.augmentaffine(df, removezerocols=true)
+        linear3, bestparams3 = PHD.regress_cv(Y, X_affine, lasso=[true], alpha=[0.7,0.8,0.9,1.0],
+                                              missing_penalty=[2.0,4.0,6.0,8.0,12.0,16.0])
+        R2, OSR2 = PHD.evaluate(Y, X_affine, linear3)
+        push!(results_table, [dname, iter, "Affine", OSR2])
+        CSV.write(savedir*filename, results_table)
     end
 end
