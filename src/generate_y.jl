95be2023152ba9a7da68e9c0ac46c43761965b89
Jean PAUPHILET <jean.pauphilet@gmail.com>
2020-06-12 18:18:49 +0200

    Update Y generation k/k_missing

--- src/generate_y.jl
+++ src/generate_y.jl
@@ -47,31 +47,36 @@ end
 		- a vector of length nrow(data) with the values of Y
 """
 function linear_y(data::DataFrame;
-	k::Real=20, SNR::Real=4,
+	k::Real=10, SNR::Real=4,
     canbemissing=falses(Base.size(data,2)), #indicates which features can be missing
     k_missing_in_signal::Int=0) #indicates the number of potentially missing features in signal
 
-    @assert soft_threshold >= 0.0
+    @assert k >= 0.0
     @assert SNR >= 0.0
 
     feature_names = names(data);
     nevermissing_features = feature_names[.!canbemissing]; missing_features = feature_names[canbemissing]
     setdiff!(nevermissing_features, [:Test, :Id]); setdiff!(missing_features, [:Test, :Id]);
 
-    n_missing_in_signal = min(n_missing_in_signal, length(missing_features))
+	k = min(k, length(setdiff(feature_names, [:Test, :Id])))
+    k_missing_in_signal = min(k_missing_in_signal, length(missing_features))
+	k_non_missing = max(k - k_missing_in_signal, 0)
+
     #Standardize
     newdata = standardize(data)
 
     Y = zeros(nrow(newdata))
-    #For nevermissing features, sample with soft thresholding
-    p1 = length(nevermissing_features)
-    w1 = softthresholding.(randn(p1), λ=soft_threshold)
-    Y += Matrix{Float64}(newdata[:,nevermissing_features])*w1
+    #For nevermissing features, choose then generate
+	if k_non_missing > 0
+		support = shuffle(nevermissing_features)[1:k_non_missing]
+		w1 = 2*rand(k_non_missing) .- 1
+		Y += Matrix{Float64}(newdata[:,support])*w1
+	end
     #For missing feautres, choose
-	@show n_missing_in_signal
-	if n_missing_in_signal > 0
-	    support = shuffle(missing_features)[1:n_missing_in_signal]
-	    w2 = randn(n_missing_in_signal)
+	@show k_missing_in_signal
+	if k_missing_in_signal > 0
+	    support = shuffle(missing_features)[1:k_missing_in_signal]
+	    w2 = 2*rand(k_missing_in_signal) .- 1
 	    Y += Matrix{Float64}(newdata[:,support])*w2
 	end
     #Add bias
@@ -79,7 +84,7 @@ function linear_y(data::DataFrame;
     #Add noise
     noise = randn(nrow(newdata)); noise .*= norm(Y) / norm(noise) / SNR
 
-    return Y .+ noise, p1+n_missing_in_signal, n_missing_in_signal
+    return Y .+ noise, k, k_missing_in_signal
 end
 
 """
