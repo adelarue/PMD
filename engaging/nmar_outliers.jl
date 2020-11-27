583056796cb045d4845b482a66fe6cccbd4162cf
Jean PAUPHILET <jean.pauphilet@gmail.com>
2020-11-27 11:48:15 +0100

    fix nmar outlier

--- engaging/nmar_outliers.jl
+++ engaging/nmar_outliers.jl
@@ -102,7 +102,7 @@ counter = 0
 	test_prop = .3
 	# test_ind = rand(nrow(X_missing)) .< test_prop ;
 
-	for iter in 1:10
+	for iter in 3:10
 		@show iter
 
         results_table = similar(results_main,0)
