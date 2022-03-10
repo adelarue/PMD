41500c7e8cf241fca653ccc3571d11258dc3c2fb
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-03-10 17:29:34 +0000

    explore issues with finite

--- engaging/script_realy.sh
+++ engaging/script_realy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -a 55-55
+#SBATCH -a 0-70
 #SBATCH -J realy
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
-#SBATCH --time=0-04:00
+#SBATCH --time=0-01:00
 srun julia realy_scripts.jl $SLURM_ARRAY_TASK_ID
