11fb1ac7eda7f5992e712abdbc8299782d73e549
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-03-14 11:30:39 +0000

    Check what is done already

--- engaging/fakey.jl
+++ engaging/fakey.jl
@@ -72,7 +72,7 @@ for ARG in ARGS
 
         test_prop = .3
         if k_missing == k_missingsignal #If not enough missing features to generate Y with k_missingsignal, abort (already done)
-            savedfiles = filter(t -> startswith(t, dname), readdir(savedir))
+            savedfiles = filter(t -> startswith(t, string(dname, "_SNR_", SNR, "_nmiss_", k_missingsignal)), readdir(savedir))
             map!(t -> split(replace(t, ".csv" => ""), "_")[end], savedfiles, savedfiles)
             @show savedfiles
             for iter in setdiff(1:10, parse.(Int, savedfiles))    
