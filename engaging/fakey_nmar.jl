4a1de0e59cdd5629a5173cb7c9afd3b9a6262020
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-03-14 11:15:01 +0000

    Check what is done already

--- engaging/fakey_nmar.jl
+++ engaging/fakey_nmar.jl
@@ -77,8 +77,11 @@ for ARG in ARGS
             test_prop = .3
 
             savedfiles = filter(t -> startswith(t, dname), readdir(savedir))
-            map!(t -> split(replace(t, ".csv" => ""), "_")[end], savedfiles, savedfiles)
+            map!(t -> replace(t, ".csv" => ""), savedfiles, savedfiles)
+            filter!(t -> parse(Int, split(t, "_")[end-1]) == k_missingsignal, savedfiles)
+            map!(t -> split(t, "_")[end], savedfiles, savedfiles)
             @show savedfiles
+
             for iter in setdiff(1:10, parse.(Int, savedfiles))    
             # for iter in 1:10                @show iter
 
