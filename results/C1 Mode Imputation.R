3cd2679f2a1c6be5b4fb42e2200705a173ced4e9
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-08-06 14:16:42 +0100

    redo fakey nmar

--- results/C1 Mode Imputation.R
+++ results/C1 Mode Imputation.R
@@ -1,10 +1,12 @@
-library(dplyr)
-library(readr)
 setwd("Dropbox (MIT)/1 - Research/PHD/results/")
-df <- read_csv("fakey/FINAL_results.csv")
-df <- read_csv("fakey_nmar/FINAL_results.csv")
-df <- read_csv("nmar_outliers/FINAL_results.csv")
-df <- read_csv("realy/FINAL_results.csv") %>% mutate(kMissing=1) #For realy only
+source("setup_script.R")
+
+df <- rbind(
+  read_csv("fakey_mar/FINAL_results.csv") %>% mutate(Setting = "1 - Syn-MAR"),
+  read_csv("fakey_nmar/FINAL_results.csv") %>% mutate(Setting = "2 - Syn-NMAR"),
+  read_csv("fakey_mar_adv/FINAL_results.csv") %>% mutate(Setting = "3 - Syn-NMAR adv"),
+  read_csv("realy/FINAL_results.csv") %>% mutate(SNR = 2, k = 10, kMissing=1, Setting = "4 - Real") 
+)
 
 #Claim 1: Mode imputation is detrimental
 dataset_list1 <- read_csv("pattern_counts_allfeat.csv") %>% 
@@ -23,33 +25,59 @@ mode_df <- merge(df, dataset_list, on='dataset') %>%
   mutate(clusterid = paste(dataset,kMissing))
 
 df %>% select(dataset) %>% unique() %>% nrow()
-
 mode_df %>% select(dataset) %>% unique() %>% nrow()
-  #  group_by(dataset, kMissing, method)
-#mode_df %>%
-#  group_by(dataset, kMissing, method) %>%
-#  summarize(freq = sum(kMissing >= 0)) %>%
-#  filter(freq < 10) %>%
-#  View()
 
-#Linear model: control for dataset and proportion of missing
-model <- lm(osr2 ~ dataset + method +kMissing, data=mode_df)
-summary(model)
 
-#Linear model: control for dataset and proportion of missing + cluster SD
-library(miceadds)
-model <- lm.cluster(osr2 ~ dataset + method +kMissing , data=mode_df, cluster=mode_df$dataset)
-summary(model)
+#Linear model: control for dataset and proportion of missing --> Get adjusted R2
+#Linear model: control for dataset and proportion of missing + cluster SD --> Get coeff estimates + SE + p-values
+#Functions to extract output from regression 
+get_coeff <- function(x){
+  round(summary(x)['methodImp-then-Reg 5','Estimate'], digits=4)
+}
+get_se <- function(x){
+  ceiling( (1e4)*summary(x)['methodImp-then-Reg 5','Std. Error']) / (1e4)
+}
+get_pvalue <- function(x){
+  signif(summary(x)['methodImp-then-Reg 5','Pr(>|t|)'], digits=2 )
+}
+get_r2 <- function(x){
+  round(summary(x)$adj.r.squared, digits=4)
+}
 
-library(reshape2)
-mode_df_wide <- dcast( mode_df %>% 
-  mutate(treatment = 1*(method=="Imp-then-Reg 5")) %>%
-  select(dataset,splitnum,kMissing,treatment,osr2), 
-  dataset+splitnum+kMissing ~ treatment, fun.aggregate = mean) 
+linearreg_analysis <- mode_df %>% 
+  nest(data = -Setting) %>% 
+  mutate(lm.model = map(data, ~lm(osr2 ~ dataset + method +kMissing, data=.)),
+         model = map(data, ~lm.cluster(osr2 ~ dataset + method +kMissing, data=., cluster="clusterid")),
+         coef = map(model, get_coeff),
+         se = map(model, get_se),
+         pvalue = map(model, get_pvalue),
+         r2 = map(lm.model, get_r2)
+         ) %>% 
+  unnest(c(coef,se,pvalue,r2)) %>%
+  arrange(Setting) %>%
+  select(Setting, coef, se, pvalue,r2)
 
-t.test(mode_df_wide$`1`, mode_df_wide$`0`, paired=TRUE, alternative="less")
+write_csv(linearreg_analysis, "ModeImpute_RegAnalysis.csv")
 
-wilcox.test(mode_df_wide$`1`, mode_df_wide$`0`, alternative="less", paired=TRUE, conf.int=TRUE)
+#Paired Wilcoxon and t-test
+mode_df_wide <- dcast( 
+  mode_df %>% 
+    mutate(treatment = 1*(method=="Imp-then-Reg 5")) %>%
+    select(Setting,dataset,splitnum,kMissing,treatment,osr2), 
+  Setting+dataset+splitnum+kMissing ~ treatment, 
+  fun.aggregate = mean) 
 
-mean(mode_df_wide$`1`-mode_df_wide$`0`, na.rm=T)
-median(mode_df_wide$`1`, na.rm=T)-median(mode_df_wide$`0`, na.rm=T)
+
+pairedtest_analysis <-mode_df_wide %>% 
+  nest(data = -Setting) %>% 
+  mutate(ttest.res = map(data, perform_ttest),
+         delta_mean = map(ttest.res, function(x) {round((x$estimate), digits=4) }),
+         ttest.pvalue = map(ttest.res, function(x) {signif(x$p.value, digits=2) }),
+         wtest.res = map(data, perform_wtest),
+         delta_median = map(wtest.res, function(x) {round((x$estimate), digits=4) }),
+         wtest.pvalue = map(wtest.res, function(x) {signif(x$p.value, digits=2) }),
+  ) %>% 
+  unnest(c(delta_mean,ttest.pvalue,delta_median,wtest.pvalue)) %>%
+  select(Setting, delta_mean,ttest.pvalue, delta_median,wtest.pvalue)
+
+write_csv(pairedtest_analysis, "ModeImpute_TestAnalysis.csv")
--- results/C2 Impute-then-regress.R
+++ results/C2 Impute-then-regress.R
@@ -1,10 +1,12 @@
-library(dplyr)
-library(readr)
 setwd("Dropbox (MIT)/1 - Research/PHD/results/")
-df <- read_csv("fakey/FINAL_results.csv")
-df <- read_csv("fakey_nmar/FINAL_results.csv")
-df <- read_csv("nmar_outliers/FINAL_results.csv")
-df <- read_csv("realy/FINAL_results.csv") %>% mutate(kMissing=1) #For realy only
+source("setup_script.R")
+
+df <- rbind(
+  read_csv("fakey_mar/FINAL_results.csv") %>% mutate(Setting = "1 - Syn-MAR"),
+  read_csv("fakey_nmar/FINAL_results.csv") %>% mutate(Setting = "2 - Syn-NMAR"),
+  read_csv("fakey_mar_adv/FINAL_results.csv") %>% mutate(Setting = "3 - Syn-NMAR adv"),
+  read_csv("realy/FINAL_results.csv") %>% mutate(SNR = 2, k = 10, kMissing=1, Setting = "4 - Real") 
+)
 
 #Claim 2: Mean impute not so bad
 dataset_list <- read_csv("pattern_counts_numonly.csv") %>% 
@@ -18,20 +20,49 @@ itr_df <- merge(df, dataset_list, on='dataset') %>%
   mutate(method = ifelse(method=="Imp-then-Reg 4", "Imp-then-Reg", method))
 
 
-#itr_df %>%
-#  group_by(dataset, kMissing, method) %>%
-#  summarize(freq = sum(kMissing >= 0)) %>%
-#  filter(freq < 10) %>%
-#  View()
+df %>% select(dataset) %>% unique() %>% nrow()
+itr_df %>% select(dataset) %>% unique() %>% nrow()
+
 
 #Linear model: control for dataset and proportion of missing
-model <- lm(osr2 ~ dataset + kMissing + method , data=itr_df)
-summary(model)
 
-#Linear model: control for dataset and proportion of missing + cluster SD
-library(miceadds)
-model <- lm.cluster(osr2 ~ dataset + kMissing + method , data=itr_df, cluster=itr_df$dataset)
-summary(model)
+get_coeff <- function(x,n="1"){
+  round(summary(x)[paste('methodImp-then-Reg',n),'Estimate'], digits=4)
+}
+get_se <- function(x,n="1"){
+  ceiling( (1e4)*summary(x)[paste('methodImp-then-Reg',n),'Std. Error']) / (1e4)
+}
+get_pvalue <- function(x,n="1"){
+  signif(summary(x)[paste('methodImp-then-Reg',n),'Pr(>|t|)'], digits=2 )
+}
+get_r2 <- function(x){
+  round(summary(x)$adj.r.squared, digits=4)
+}
+
+linearreg_analysis <- itr_df %>% 
+  nest(data = -Setting) %>% 
+  mutate(lm.model = map(data, ~lm(osr2 ~ dataset + method +kMissing, data=.)),
+         model = map(data, ~lm.cluster(osr2 ~ dataset + method +kMissing, data=., cluster="dataset")),
+         coef1 = map(model, function(x){get_coeff(x,n="1")}),
+         se1 = map(model, function(x){get_se(x,n="1")}),
+         pvalue1 = map(model, function(x){get_pvalue(x,n="1")}),
+         
+         coef2 = map(model, function(x){get_coeff(x,n="2")}),
+         se2 = map(model, function(x){get_se(x,n="2")}),
+         pvalue2 = map(model, function(x){get_pvalue(x,n="2")}),         
+         
+         coef3 = map(model, function(x){get_coeff(x,n="3")}),
+         se3 = map(model, function(x){get_se(x,n="3")}),
+         pvalue3 = map(model, function(x){get_pvalue(x,n="3")}),
+         
+         r2 = map(lm.model, get_r2)
+  ) %>% 
+  unnest(c(coef1,se1,pvalue1, coef2,se2,pvalue2, coef3,se3,pvalue3, r2)) %>%
+  arrange(Setting) %>%
+  select(Setting, coef1,se1,pvalue1, coef2,se2,pvalue2, coef3,se3,pvalue3, r2)
+
+write_csv(linearreg_analysis, "ImputeThenReg_RegAnalysis.csv")
+
 
 #Paired t- and Wilson-tests V2 vs V3
 library(reshape2)
@@ -40,47 +71,56 @@ method1 = "Imp-then-Reg 2"
 itr_df_wide <- dcast( itr_df %>% 
                         mutate(treatment = 1*(method==method1), control=1*(method==method0)) %>%
                         filter(treatment+control > 0) %>%
-                        select(dataset,kMissing,splitnum,treatment,osr2), 
-                       dataset+splitnum+kMissing~ treatment, fun.aggregate = mean) 
+                        select(Setting,dataset,kMissing,splitnum,treatment,osr2), 
+                      Setting+dataset+splitnum+kMissing~ treatment, 
+                      fun.aggregate = mean) 
 
-t.test(itr_df_wide$`1`, itr_df_wide$`0`, paired=TRUE)
-mean(itr_df_wide$`1`-itr_df_wide$`0`, na.rm=T)
+pairedtest_analysis <-itr_df_wide %>% 
+  nest(data = -Setting) %>% 
+  mutate(ttest.res = map(data, perform_ttest),
+         delta_mean = map(ttest.res, function(x) {round((x$estimate), digits=4) }),
+         ttest.pvalue = map(ttest.res, function(x) {signif(x$p.value, digits=2) }),
+         wtest.res = map(data, perform_wtest),
+         delta_median = map(wtest.res, function(x) {round((x$estimate), digits=4) }),
+         wtest.pvalue = map(wtest.res, function(x) {signif(x$p.value, digits=2) })
+  ) %>% 
+  unnest(c(delta_mean,ttest.pvalue,delta_median,wtest.pvalue)) %>%
+  select(Setting, delta_mean,ttest.pvalue, delta_median,wtest.pvalue)
 
-wilcox.test(itr_df_wide$`1`, itr_df_wide$`0`, paired=TRUE, conf.int=TRUE)
-median(itr_df_wide$`1`, na.rm=T) -median(itr_df_wide$`0`, na.rm=T)
+
+write_csv(pairedtest_analysis, "ImputeThenReg_2vs3_TestAnalysis.csv")
 
 
 #Paired t- and Wilson-tests V4 vs Vx
-library(reshape2)
 method0 = "Imp-then-Reg" #Same as "Imp-then-Reg 4"
-method1 = "Imp-then-Reg 3"
-itr_df_wide <- dcast( itr_df %>% 
-                        mutate(treatment = 1*(method==method1), control=1*(method==method0)) %>%
-                        filter(treatment+control > 0) %>%
-                        select(dataset,kMissing,splitnum,treatment,osr2), 
-                      dataset+splitnum+kMissing~ treatment, fun.aggregate = mean) 
 
-t.test(itr_df_wide$`1`, itr_df_wide$`0`, paired=TRUE)
-mean(itr_df_wide$`1`-itr_df_wide$`0`, na.rm=T)
-
-wilcox.test(itr_df_wide$`1`, itr_df_wide$`0`, paired=TRUE, conf.int=TRUE)
-median(itr_df_wide$`1`, na.rm=T) -median(itr_df_wide$`0`, na.rm=T)
-
-
-
+for (i in 1:3){
+  method1 = paste("Imp-then-Reg", i)
+  
+  itr_df_wide <- dcast( itr_df %>% 
+                          mutate(treatment = 1*(method==method1), control=1*(method==method0)) %>%
+                          filter(treatment+control > 0) %>%
+                          select(Setting,dataset,kMissing,splitnum,treatment,osr2), 
+                        Setting+dataset+splitnum+kMissing~ treatment, 
+                        fun.aggregate = mean) 
+  
+  pairedtest_analysis <-itr_df_wide %>% 
+    nest(data = -Setting) %>% 
+    mutate(ttest.res = map(data, perform_ttest),
+           delta_mean = map(ttest.res, function(x) {round((x$estimate), digits=4) }),
+           ttest.pvalue = map(ttest.res, function(x) {signif(x$p.value, digits=2) }),
+           wtest.res = map(data, perform_wtest),
+           delta_median = map(wtest.res, function(x) {round((x$estimate), digits=4) }),
+           wtest.pvalue = map(wtest.res, function(x) {signif(x$p.value, digits=2) })
+    ) %>% 
+    unnest(c(delta_mean,ttest.pvalue,delta_median,wtest.pvalue)) %>%
+    select(Setting, delta_mean,ttest.pvalue, delta_median,wtest.pvalue)
+  
+  
+  write_csv(pairedtest_analysis, paste("ImputeThenReg_",i,"vs4_TestAnalysis.csv", sep=""))
+}
 
 #Computational time
-df <- read_csv("fakey/FINAL_results.csv") %>%  
-  mutate(setting = "1 - Syn MAR") %>% 
-  select(-SNR,-k,-kMissing,)
-df <- rbind(df, read_csv("fakey_nmar/FINAL_results.csv") %>%  
-  mutate(setting = "2 - Syn NMAR") %>% 
-  select(-SNR,-k,-kMissing))
-df <- rbind(df, read_csv("nmar_outliers/FINAL_results.csv") %>%  
-  mutate(setting = "3 - Syn MAR adv") %>% 
-  select(-SNR,-k,-kMissing))
-df <- rbind(df, read_csv("realy/FINAL_results.csv") %>%  mutate(setting = "4 - Real"))
-
 library(ggplot2)
 library(stringr)
 
@@ -90,9 +130,9 @@ df %>%
   filter(method < "Imp-then-Reg 5") %>%
   #filter(splitnum <= 10) %>%
   mutate(Method = str_replace(method, "Imp-then-Reg ", "V")) %>%
-  group_by(Method, setting) %>%
+  group_by(Method, Setting) %>%
   summarize(time = mean(time)) %>%
-  ggplot() + aes(x=setting, y=time, fill=Method, color=Method) + 
+  ggplot() + aes(x=Setting, y=time, fill=Method, color=Method) + 
   geom_col(position="dodge",alpha=0.7) +
   labs(x="Setting", y = "Average time (in seconds)") +
   scale_fill_brewer(palette="Set1") + scale_color_brewer(palette="Set1") +
@@ -105,3 +145,4 @@ df %>%
     #legend.box.margin = margin(0, 0, 0, 0),
     legend.spacing.y = unit(2, "line"))
 
+ggsave("ImputeThenReg_Time.png",  width = 7, height = 5, dpi = 300)
