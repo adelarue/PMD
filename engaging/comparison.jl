341143f1a6ff80c04855c2b33a486bea46336d5d
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-08-25 12:08:54 +0100

    redo syn nn

--- engaging/comparison.jl
+++ engaging/comparison.jl
@@ -19,7 +19,7 @@ ktotal = 5
 # missingness_proba_list = collect(0.05:0.05:0.9)
 # missingness_proba_list = collect(0.1:0.1:0.9)
 # missingness_proba_list = collect(0.2:0.1:0.9)
-missingness_proba_list = collect(0.6:0.1:0.9)
+missingness_proba_list = collect(0.4:0.1:0.9)
 
 # missingness_proba = 0.1 
 num_missing_feature = p 
--- engaging/script_syn_nmar.sh
+++ engaging/script_syn_nmar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -a 30-49
+#SBATCH -a 0-49
 #SBATCH -J syn_lin_nmar
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
-#SBATCH --time=1-12:00
-srun julia comparison.jl $SLURM_ARRAY_TASK_ID 0 1
+#SBATCH --time=2-00:00
+srun julia comparison.jl $SLURM_ARRAY_TASK_ID 0 3
