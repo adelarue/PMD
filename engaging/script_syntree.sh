4a1be0706bcb88010a27d9d958b9f86cade154cb
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-08-25 12:05:28 +0100

    Redo tree syn

--- engaging/script_syntree.sh
+++ engaging/script_syntree.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -a 0-49
+#SBATCH -a 30-49
 #SBATCH -J syn_tree_mar
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
-#SBATCH --time=0-12:00
+#SBATCH --time=1-12:00
 srun julia comparison.jl $SLURM_ARRAY_TASK_ID 1 2
