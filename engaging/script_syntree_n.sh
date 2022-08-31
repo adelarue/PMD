2d3bc8af82be40bfda9ad11d14b062cd2acc2951
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-08-31 15:05:51 +0100

    add scripts for syn x/y all methods

--- engaging/script_syntree_n.sh
+++ engaging/script_syntree_n.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -a 30-49
-#SBATCH -J syn_tree_nmar
+#SBATCH -a 0-49
+#SBATCH -J s_tree_nmar
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
 #SBATCH --time=0-12:00
-srun julia comparison.jl $SLURM_ARRAY_TASK_ID 0 2
+srun julia comparison_full.jl $SLURM_ARRAY_TASK_ID 0 2
