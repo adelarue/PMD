39a16245a8985b510a04b5126fed5d6ee9fe0ded
Jean PAUPHILET <jean.pauphilet@gmail.com>
2022-08-31 15:05:51 +0100

    add scripts for syn x/y all methods

--- engaging/script_synlin.sh
+++ engaging/script_synlin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -a 0-49
-#SBATCH -J syn_lin_nmar
+#SBATCH -J s_lin_mar
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
-#SBATCH --time=2-00:00
-srun julia comparison.jl $SLURM_ARRAY_TASK_ID 0 3
+#SBATCH --time=0-12:00
+srun julia comparison_full.jl $SLURM_ARRAY_TASK_ID 1 1
--- engaging/script_synnn.sh
+++ engaging/script_synnn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -a 0-49
-#SBATCH -J s_tree_mar
+#SBATCH -J s_nn_mar
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
 #SBATCH --time=0-12:00
-srun julia comparison_full.jl $SLURM_ARRAY_TASK_ID 1 2
+srun julia comparison_full.jl $SLURM_ARRAY_TASK_ID 1 3
