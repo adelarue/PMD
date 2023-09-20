dc5e0c1d7d81dae205566e1b15d4b484dabdea46
jeanpauphilet <jean.pauphilet@gmail.com>
2023-09-20 15:28:04 +0100

    sun data exp for mode impute

--- engaging/script_synnn_nmar.sh
+++ engaging/script_synnn_nmar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -a 21-49
+#SBATCH -a 0-49
 #SBATCH -J s_nn_nmar
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
--- engaging/script_syntree_n.sh
+++ engaging/script_syntree_n.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -a 21-49
+#SBATCH -a 0-49
 #SBATCH -J s_tree_nmar
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
-#SBATCH --time=0-00:30
+#SBATCH --time=0-01:00
 srun julia synthetic_discrete.jl $SLURM_ARRAY_TASK_ID 0 2
