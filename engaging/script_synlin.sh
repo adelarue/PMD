7c146c415b3cc8bbc276b152d188a1f5400cb604
jeanpauphilet <jean.pauphilet@gmail.com>
2023-09-20 15:27:00 +0100

    sun data exp for mode impute

--- engaging/script_synlin.sh
+++ engaging/script_synlin.sh
@@ -4,5 +4,5 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH -p sched_mit_sloan_batch
-#SBATCH --time=0-00:30
+#SBATCH --time=0-01:00
 srun julia synthetic_discrete.jl $SLURM_ARRAY_TASK_ID 1 1
