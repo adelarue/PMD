543aa42859d8dc6c2f97a2b96bb10aa23b2404c6
jeanpauphilet <jean.pauphilet@gmail.com>
2023-11-28 16:21:20 +0000

    update packages

---
+++ engaging/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -a 0-9
+#SBATCH -J pkg
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+#SBATCH -p sched_mit_sloan_batch
+#SBATCH --time=1-00:00
+srun pkg_updates.jl $SLURM_ARRAY_TASK_ID
